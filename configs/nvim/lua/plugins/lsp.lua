bb06fa16bd10a64a4044c5a6c66b28c911e5e9a9
Humberto Lima <hlaff@cin.ufpe.br>
2026-08-08 01:51:37 -0300

    feat(nvim): configure pyright to automatically resolve virtual environments

--- configs/nvim/lua/plugins/lsp.lua
+++ configs/nvim/lua/plugins/lsp.lua
@@ -2,5 +2,21 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     inlay_hints = { enabled = false },
+    servers = {
+      pyright = {
+        settings = {
+          python = {
+            venvPath = ".",
+            venv = ".venv",
+            analysis = {
+              typeCheckingMode = "basic",
+              autoSearchPaths = true,
+              useLibraryCodeForTypes = true,
+              autoImportCompletions = true,
+            },
+          },
+        },
+      },
+    },
   },
 }
